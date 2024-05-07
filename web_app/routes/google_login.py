2f9ec24df3c35b18b77a4ae6711edcea8740cdcf
khaliwalii <143511676+khaliwalii@users.noreply.github.com>
2024-05-07 15:13:18 -0400

    JSOW altercation

--- web_app/routes/google_login.py
+++
@@ -1,9 +0,0 @@
-from web_app.routes.auth_routes import auth_routes
-
-
-@auth_routes.route("/auth/google/login")
-def google_login():
-    print("GOOGLE OAUTH LOGIN...")
-    oauth = current_app.config["OAUTH"]
-    redirect_uri = url_for("auth_routes.google_oauth_callback", _external=True) # see corresponding route below
-    return oauth.google.authorize_redirect(redirect_uri) # send the user to login with google, then hit the callback route
